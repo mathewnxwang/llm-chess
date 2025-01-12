e8192bc0c630705759f9e0a14aa33a3569e03bd3
Mathew Wang <mathewnxwang@gmail.com>
2025-01-11 22:36:03 -0500

    adding llm player

--- chess_engine.py
+++ chess_engine.py
@@ -3,6 +3,8 @@ import chess.engine
 import chess.pgn
 import io
 
+from llm_manager import LLMManager
+
 class ChessEngine:
 
     def __init__(self, skill_level: int):
@@ -19,34 +21,80 @@ class ChessEngine:
         result = self.engine.play(board, chess.engine.Limit(time=time_limit))
         return result.move
 
-    def get_and_execute_user_move(self, board: chess.Board) -> None:
-
-        while True:
-
-            move_str = input("Enter your move: ")
-
-            try:
-                move: chess.Move = board.parse_san(move_str)
-                break
-            except chess.InvalidMoveError:
-                print("Invalid move notation. Try again.")
-                continue
-            except chess.IllegalMoveError:
-                print("Illegal move. Try again.")
-                continue
-            except chess.AmbiguousMoveError:
-                print("Ambiguous move. Try again.")
-                continue
-
-        board.push(move)
-
     def close(self):
         self.engine.quit()
 
+
+def get_and_execute_user_move(board: chess.Board) -> None:
+
+    while True:
+
+        move_str = input("Enter your move: ")
+
+        try:
+            move: chess.Move = board.parse_san(move_str)
+            break
+        except chess.InvalidMoveError:
+            print("Invalid move notation. Try again.")
+            continue
+        except chess.IllegalMoveError:
+            print("Illegal move. Try again.")
+            continue
+        except chess.AmbiguousMoveError:
+            print("Ambiguous move. Try again.")
+            continue
+
+    board.push(move)
+
+
+def get_and_execute_llm_move(llm_manager: LLMManager, board: chess.Board, position: str) -> None:
+
+    retries = 3
+    error_message = None
+    while retries > 0:
+
+        move_str = llm_manager.make_llm_move(position=position, error_message=error_message)
+
+        try:
+            move: chess.Move = board.parse_san(move_str)
+            break
+        except chess.InvalidMoveError:
+            error_message = f"Invalid move notation: '{move_str}'."
+            print(error_message)
+            retries -= 1
+            continue
+        except chess.IllegalMoveError:
+            error_message = f"Illegal move: '{move_str}'."
+            print(error_message)
+            retries -= 1
+            continue
+        except chess.AmbiguousMoveError:
+            error_message = f"Ambiguous move: '{move_str}'."
+            print(error_message)
+            retries -= 1
+            continue
+
+    if retries == 0:
+        raise Exception("Failed to get a valid LLM move after 3 attempts.")
+
+    board.push(move)
+
+
+def convert_board_to_pgn(board: chess.Board) -> str:
+    pgn = chess.pgn.Game()
+    pgn.setup(board)
+    pgn_string = io.StringIO()
+    exporter = chess.pgn.FileExporter(pgn_string)
+    game.accept(exporter)
+
+    print("PGN string for the LLM: \n" + pgn_string.getvalue())
+    return pgn_string.getvalue()
+
+
 if __name__ == "__main__":
     engine = ChessEngine(skill_level=10)
-    weak_engine = ChessEngine(skill_level=1)
     board = chess.Board()
+    llm_manager = LLMManager()
 
     game = chess.pgn.Game()
     node = game
@@ -59,17 +107,19 @@ if __name__ == "__main__":
             print(f"Engine's move: {move}")
 
         else:
-            move = weak_engine.get_best_move(board)
-            board.push(move)
-            print(f"Weak engine's move: {move}")
-            # engine.get_and_execute_user_move(board)
+            # get_and_execute_user_move(board)
+            position = convert_board_to_pgn(board)
+            get_and_execute_llm_move(
+                llm_manager=LLMManager(),
+                board=board,
+                position=position
+            )
 
         node = node.add_variation(board.move_stack[-1])
 
-        print(board)
+        print(f"Current board: \n{board}\n")
     
     engine.close()
-    weak_engine.close()
 
     print("Game over.")
     print(board.result())
--- llm_manager.py
+++ llm_manager.py
@@ -6,17 +6,41 @@ from enum import Enum
 from openai import OpenAI
 from openai.types.chat.chat_completion import ChatCompletion
 
+SYSTEM_PROMPT = """You are a grandmaster chess player.
+Given the position in PGN format, return the best, valid next move in standard algebraic notation.
+Example of a correct response: 'e5'
+Examples of incorrect responses:
+- '2. e5'
+- 'e5 is the best move to play in this position.'""" 
+
+USER_PROMPT = """Position: {position}
+
+Black to play.
+
+For the love of god please don't add prefixes like '2.' or '2... ' to your move.
+
+Move: """
+
+USER_PROMPT_WITH_ERROR = """
+Position: {position}
+
+Black to play.
+
+Make sure to avoid this error: {error_message}
+
+For the love of god please don't add prefixes like '2.' or '2... ' to your move.
+
+Move: """
+
 class LLMManager():
     def __init__(self):
         load_dotenv("secrets.env")
         openai_api_key = os.getenv("OPENAI_API_KEY")
-        print("openai_api_key should be defined and this should print before the error")
-        print(openai_api_key)
         self.client = OpenAI(api_key=openai_api_key)
 
     def call_llm(
             self,
-            model: LLMModel,
+            model: str,
             system_prompt: str,
             user_prompt: str,
             temperature: float = 0
@@ -27,10 +51,25 @@ class LLMManager():
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt}
             ],
-            model=model.value,
+            model=model,
             temperature=temperature,
-            max_tokens=300
+            # max_tokens=300
         )
         message = response.choices[0].message.content
-        print(response)
+        print(f"LLM response: {message}")
         return message
+
+    def make_llm_move(self, position: str, error_message: str | None) -> str:
+
+        if error_message:
+            formatted_user_prompt = USER_PROMPT_WITH_ERROR.format(position=position, error_message=error_message)
+        else:
+            formatted_user_prompt = USER_PROMPT.format(position=position)
+        
+        response = self.call_llm(
+            model="gpt-4o",
+            system_prompt=SYSTEM_PROMPT,
+            user_prompt=formatted_user_prompt
+        )
+
+        return response
